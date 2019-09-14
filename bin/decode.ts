2a6e7da40c592e586912934991d48ec9356c9223
Joshua May <notjosh@gmail.com>
2019-09-13 18:22:22 -0700

    Decoder prints to CLI when possible

--- bin/decode.ts
+++ bin/decode.ts
@@ -16,6 +16,8 @@ import decoder from '../src/decoder';
 import fs from 'fs';
 import { promisify } from 'util';
 
+import termImg from 'term-img';
+
 const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
 
@@ -27,32 +29,6 @@ if (path == null) {
   process.exit(1);
 }
 
-// const input = [1, 2, 3, 1000000, 10000, 100000];
-
-// const output = [] as number[];
-
-// input.forEach((item, idx) => {
-//   // evens are white:
-//   const value = idx % 2 == 0 ? 1 : 0;
-
-//   const run = Array(item).fill(value);
-
-//   console.log({ idx, item, value }, output.length + run.length);
-
-//   // doin' some fun hacks, since `output.push(...run);` will overflow on longlonglong runs
-//   const length = output.length;
-//   output.length += run.length;
-
-//   for (let i = 0; i < run.length; i++) {
-//     output[length + i] = run[i];
-//   }
-// });
-
-// console.log(output.length);
-// input.forEach((item, idx) => {});
-
-// return output;
-
 (async () => {
   const string = await readFile(path);
   const json = JSON.parse(string.toString('utf-8'));
@@ -62,4 +38,8 @@ if (path == null) {
 
   await writeFile('out.bmp', result.payload.bitmap);
   console.log('wrote to out.bmp');
+
+  termImg(result.payload.bitmap);
+
+  return Promise.resolve();
 })();
