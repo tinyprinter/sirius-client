696d6b0ee5cfca9319686b07f8d4c8b167646545
KTamas <ktamas@ktamas.com>
2020-05-11 14:04:37 +0200

    Adapt filesystem driver

--- src/printer-driver/filesystem-printer.ts
+++ src/printer-driver/filesystem-printer.ts
@@ -1,14 +1,14 @@
-import Printer from '.';
-import { CommandResponse } from '../../types';
+import { PrinterDriverInterface, PrintingResult } from '.';
 import fs from 'fs';
+// import bitmapify from '../decoder/parser/bitmapify';
+// import termImg from 'term-img';
 
-import termImg from 'term-img';
-
-export default class FilesystemPrinter extends Printer {
-  async print(buffer: Buffer): Promise<CommandResponse> {
+export default class FilesystemPrinterDriver implements PrinterDriverInterface {
+  async print(buffer: Buffer): Promise<PrintingResult> {
     return new Promise(resolve => {
       fs.writeFileSync('/root/tmp/to_print.bmp', buffer);
       console.log("Written.");
+      // const bitmap = bitmapify(buffer);
       //termImg(buffer);
       resolve();
     });
