2da8c3c82734459b5892401948333dbaf8b651d5
Emory Dunn <edunn@emorydunn.com>
2020-05-11 12:28:03 -0700

    Write file to the system temp dir

--- src/printer-driver/filesystem-printer.ts
+++ src/printer-driver/filesystem-printer.ts
@@ -1,13 +1,20 @@
 import { PrinterDriverInterface, PrintingResult } from '.';
 import fs from 'fs';
+import os from 'os';
+import path from 'path';
 // import bitmapify from '../decoder/parser/bitmapify';
 // import termImg from 'term-img';
 
 export default class FilesystemPrinterDriver implements PrinterDriverInterface {
   async print(buffer: Buffer): Promise<PrintingResult> {
     return new Promise(resolve => {
-      fs.writeFileSync('/root/tmp/to_print.bmp', buffer);
-      console.log('Written.');
+      let tempDir = path.join(os.tmpdir(), 'sirius-client');
+      fs.mkdirSync(tempDir, {'recursive': true});
+
+      let tempFile = path.join(tempDir, 'to_print.bmp');
+
+      fs.writeFileSync(tempFile, buffer);
+      console.log(`Written: ${tempFile}`);
       // const bitmap = bitmapify(buffer);
       //termImg(buffer);
       resolve();
