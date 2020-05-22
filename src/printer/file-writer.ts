4e2658ff004def463a7bcc851def92808c2af227
Joshua May <notjosh@gmail.com>
2020-05-22 18:28:05 +0200

    Added file_writer print handler

---
+++ src/printer/file-writer.ts
@@ -0,0 +1,104 @@
+import { promises as fs, mkdir } from 'fs';
+import Path from 'path';
+import { BergPrinterPayload } from '../berger/device/printer/payload-decoder';
+import PrintableImage from '../printable-image';
+import { PrintableImageHandler } from './printable-image-wrapper';
+import { PrinterParameters } from '../configuration';
+import { is, assertType } from 'typescript-is';
+
+enum Format {
+  BIN = 'bin',
+  PNG = 'png',
+}
+
+export type FileWriterParameters = {
+  image: {
+    width: number;
+  };
+  directory: string;
+  format: Format;
+};
+
+export default class implements PrintableImageHandler {
+  static type = 'file_writer';
+
+  private parameters: FileWriterParameters;
+
+  constructor(parameters: FileWriterParameters) {
+    this.parameters = parameters;
+  }
+
+  static areParametersValid(parameters: PrinterParameters): boolean {
+    return is<FileWriterParameters>(parameters);
+  }
+
+  static fromParameters(parameters: PrinterParameters): PrintableImageHandler {
+    return new this(assertType<FileWriterParameters>(parameters));
+  }
+
+  async open(): Promise<void> {
+    this.mkdir(this.parameters.directory);
+  }
+
+  async close(): Promise<void> {}
+
+  async print(
+    image: PrintableImage,
+    payload: BergPrinterPayload
+  ): Promise<boolean> {
+    try {
+      // if we've come from a payload, it must be upside down
+      if (payload != null) {
+        image.rotate(180);
+      }
+
+      image.resize(this.parameters.image.width);
+
+      const buffer = await this.bufferise(image);
+
+      // TODO: obvioussssly a name based on date this won't play well with a
+      // lot of connections, but it's fine for now
+      const now = new Date();
+      const filename = `${now.toISOString().replace(/[^0-9]/g, '-')}.${
+        this.parameters.format
+      }`;
+
+      const path = Path.join(this.parameters.directory, filename);
+
+      console.log(`writing to: ${path}`);
+
+      await fs.writeFile(path, buffer);
+
+      return true;
+    } catch (error) {
+      console.log(`FileWriter error: ${error}`);
+      return false;
+    }
+  }
+
+  private async mkdir(path: string): Promise<void> {
+    try {
+      await fs.mkdir(path);
+    } catch (error) {
+      if (error.code === 'EEXIST') {
+        // Something already exists, but is it a file or directory?
+        const lstat = await fs.lstat(path);
+
+        if (!lstat.isDirectory()) {
+          throw error;
+        }
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  private async bufferise(image: PrintableImage): Promise<Buffer> {
+    switch (this.parameters.format) {
+      case Format.PNG:
+        return await image.asPNG();
+      case Format.BIN:
+        return await image.asBIN();
+    }
+  }
+}
--- src/printer/index.ts
+++ src/printer/index.ts
@@ -1,11 +1,13 @@
 import Console from './console';
 import EscPos from './escpos';
+import FileWriter from './file-writer';
 import Paperang from './paperang';
 import Peripage from './peripage';
 
 const all = {
   [Console.type]: Console,
   [EscPos.type]: EscPos,
+  [FileWriter.type]: FileWriter,
   [Paperang.type]: Paperang,
   [Peripage.type]: Peripage,
 };
